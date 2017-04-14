e7964140cbb0ee7badfc3d67c6412f5c3a461f76
Roman Bataev <roman.bataev@gmail.com>
2017-04-13 22:35:21 -0700

    Update comments

--- flycheck-joker.el
+++ flycheck-joker.el
@@ -9,12 +9,12 @@
 
 ;;; Commentary:
 
-;; This package adds Clojure syntax checker (via Joker) to flycheck. To use it, add
+;; This package adds Clojure syntax checker (via Joker) to flycheck.  To use it, add
 ;; to your init.el:
 
 ;; (require 'flycheck-joker)
 
-;; Make sure Joker executable is on your path. The latest version
+;; Make sure Joker executable is on your path.  The latest version
 ;; of Joker can be downloaded from https://github.com/candid82/joker/releases
 
 ;;; License:
