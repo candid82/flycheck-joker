0d8d5683a273093ca12841bf93d10dae97ccbc5d
Roman Bataev <roman@bataev.me>
2018-07-12 21:02:54 -0700

    Exclude edn from clojure-joker

--- flycheck-joker.el
+++ flycheck-joker.el
@@ -57,7 +57,8 @@
   :error-patterns
   ((error line-start "<stdin>:" line ":" column ": " (0+ not-newline) (or "error: " "Exception: ") (message) line-end)
    (warning line-start "<stdin>:" line ":" column ": " (0+ not-newline) "warning: " (message) line-end))
-  :modes (clojure-mode clojurec-mode))
+  :modes (clojure-mode clojurec-mode)
+  :predicate (lambda () (not (string= "edn" (file-name-extension (buffer-file-name))))))
 
 (flycheck-define-checker clojurescript-joker
   "A ClojureScript syntax checker using Joker.
@@ -70,7 +71,7 @@
    (warning line-start "<stdin>:" line ":" column ": " (0+ not-newline) "warning: " (message) line-end))
   :modes (clojurescript-mode))
 
-(flycheck-define-checker clojure-joker-edn
+(flycheck-define-checker edn-joker
   "EDN syntax checker using Joker.
   See URL `https://github.com/candid82/joker'."
   :command ("joker" "--lintedn" "--")
@@ -83,7 +84,7 @@
 
 (add-to-list 'flycheck-checkers 'clojure-joker)
 (add-to-list 'flycheck-checkers 'clojurescript-joker)
-(add-to-list 'flycheck-checkers 'clojure-joker-edn)
+(add-to-list 'flycheck-checkers 'edn-joker)
 
 (provide 'flycheck-joker)
 ;;; flycheck-joker.el ends here
